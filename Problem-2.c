614af383d28cb428552ac785e5897670298669e2
Carson Bavery <ctrackb31@gmail.com>
2024-06-21 01:18:06 -0400

    Initial commit

--- Problem-2.c
+++ Problem-2.c
@@ -3,12 +3,78 @@
   Then, after you’ve gotten this version working, modify your program so that it can handle any comm sz.
 **/
 
-
-#include <stdio.h>
 #include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+void initialize_mpi(int *comm_sz, int *my_rank);
+int compute_tree_sum(int my_rank, int comm_sz, int local_value);
+void handle_remainder(int *sum, int my_rank, int comm_sz);
+void finalize_mpi();
 
-void main()
-{
+int main(int argc, char** argv) {
+    int comm_sz, my_rank;
+    int local_value, global_sum;
 
+    MPI_Init(&argc, &argv);
+    initialize_mpi(&comm_sz, &my_rank);
+
+    local_value = my_rank;
+
+    global_sum = compute_tree_sum(my_rank, comm_sz, local_value);
+
+    handle_remainder(&global_sum, my_rank, comm_sz);
+
+    if (my_rank == 0) {
+        printf("The global sum is %d\n", global_sum);
+    }
+
+    finalize_mpi();
+    return 0;
+}
+
+void initialize_mpi(int *comm_sz, int *my_rank) {
+    MPI_Comm_size(MPI_COMM_WORLD, comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, my_rank);
+}
+
+int compute_tree_sum(int my_rank, int comm_sz, int local_value) {
+    int sum = local_value;
+    int step;
+
+    for (step = 1; step < comm_sz; step *= 2) {
+        if (my_rank % (2 * step) == 0) {
+            int partner = my_rank + step;
+            if (partner < comm_sz) {
+                int received_value;
+                MPI_Recv(&received_value, 1, MPI_INT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                sum += received_value;
+            }
+        } else if (my_rank % step == 0) {
+            int partner = my_rank - step;
+            MPI_Send(&sum, 1, MPI_INT, partner, 0, MPI_COMM_WORLD);
+            break;
+        }
+    }
+    return sum;
+}
+
+void handle_remainder(int *sum, int my_rank, int comm_sz) {
+    int remaining_processes = comm_sz;
+
+    if (remaining_processes % 2 != 0) {
+        if (my_rank == remaining_processes - 1) {
+            MPI_Send(sum, 1, MPI_INT, my_rank - 1, 0, MPI_COMM_WORLD);
+        }
+        if (my_rank == remaining_processes - 2) {
+            int received_value;
+            MPI_Recv(&received_value, 1, MPI_INT, my_rank + 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            *sum += received_value;
+        }
+        remaining_processes--;
+    }
+}
+
+void finalize_mpi() {
+    MPI_Finalize();
 }
